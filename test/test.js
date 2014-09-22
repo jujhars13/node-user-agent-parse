48c630c5768359dd578adb05709cbb858d7ec5c1
Jujhar Singh <jujhar@jujhar.com>
2014-09-22 14:05:45 +0100

    updating tests

--- test/test.js
+++ test/test.js
@@ -9,7 +9,7 @@ var user_agent = {
     safari: "Mozilla/5.0 (Linux; U; Android 4.0.3; de-ch; HTC Sensation Build/IML74K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
     iphone: "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16",
     ie: "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Win64; x64; Trident/6.0)"
-};
+}
 
 _(user_agent).forEach(function(v, k) {
     console.log(parse.parse(v));
