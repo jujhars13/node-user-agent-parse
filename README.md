dabf36ef34fec8aedeeb6d2d8a1a58b13c83ceaa
Jujhar Singh <jujhar@jujhar.com>
2019-11-18 09:19:02 +0000

    cleaned up tests, changed licence to MIT

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Node User Agent Parse
 
+Very simple, no deps user agent string parser.
+
 [![NPM](https://nodei.co/npm/user-agent-parse.png?downloads=true)](https://nodei.co/npm/user-agent-parse/)
 
 ## Build Status
 [![Build Status](https://travis-ci.org/jujhars13/node-user-agent-parse.png?branch=master)](https://travis-ci.org/jujhars13/node-user-agent-parse)
-
 
 ## Overview
 
@@ -13,10 +14,15 @@ Rudimentary but it works.
 
 ## Example
 ```javascript
-     var userAgent = require('user-agent')
-     userAgent.parse('Mozilla/5.0 (Windows; U; Windows NT 5.1; en) AppleWebKit/526.9 (KHTML, like Gecko) Version/4.0dp1 Safari/526.8')
+     const userAgent = require('user-agent');
+     userAgent.parse('Mozilla/5.0 (Windows; U; Windows NT 5.1; en) AppleWebKit/526.9 (KHTML, like Gecko) Version/4.0dp1 Safari/526.8');
      // => { name: 'safari', version: '4.0dp1', os: 'Windows XP', full: '... same string as above ...', device_type:'desktop' }
 ```
+
+## Tests
+
+See '/test/test.js` for a simple suite
+
 ## Credits
 
 Heavily based on (blatantly ripped from *ahem*) https://github.com/soldair/node-ua-device-type and https://github.com/jujhars13/node-user-agent-parse
@@ -24,6 +30,6 @@ Heavily based on (blatantly ripped from *ahem*) https://github.com/soldair/node-
 ## TODO
 Implemnet tests using `mocha` and `should`
 
-##License
+## License
 
-License is [MIT](http://opensource.org/licenses/mit-license.php), go nuts.
+License is [MIT](LICENCE.md), go nuts.
--- test/test.js
+++ test/test.js
@@ -1,16 +1,19 @@
 /**
- * quick and dirty test
- * TODO rewrite using vows
+ * quick test
  */
-var parse = require('../lib/user-agent-parse');
-var _ = require('lodash');
-var user_agent = {
-    firefox: "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14",
-    safari: "Mozilla/5.0 (Linux; U; Android 4.0.3; de-ch; HTC Sensation Build/IML74K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
-    iphone: "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16",
-    ie: "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Win64; x64; Trident/6.0)"
-}
+const parser = require('../lib/user-agent-parse');
+const assert = require('assert');
 
-_(user_agent).forEach(function(v, k) {
-    console.log(parse.parse(v));
+const userAgents = {
+  firefox: 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14',
+  safari: 'Mozilla/5.0 (Linux; U; Android 4.0.3; de-ch; HTC Sensation Build/IML74K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
+  chrome: 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
+  msie: 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Win64; x64; Trident/6.0)',
+  edge: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
+};
+
+Object.entries(userAgents).forEach((v) => {
+  const browser = v[0];
+  const response = parser.parse(v[1]);
+  assert.strictEqual(browser, response.name, 'browser name does not match');
 });
